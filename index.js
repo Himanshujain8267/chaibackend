747bf8a93e0d8f4d2dc59acb806b5e75e0e9236e
himanshu jain <himanshujain8267@gmail.com>
2025-03-29 16:29:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 5000
+const githubdata = {
+   
+        "message": "Not Found",
+        "documentation_url": "https://docs.github.com/rest",
+        "status": "404"
+      
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter',(req , res)=>{
+res.send('himanshudotcom')
+}) 
+app.get('/login',(req,res)=>{
+    res.send('<h1>please login at chai or code </h1>')
+})
+app.get('/chaiorcode',(req,res)=>{
+    res.send('hloeveryone')
+})
+app.get('/hloeveryone',(req,res)=>{
+    res.send('hlo_everyone')
+})
+app.get('/github' , (req , res)=>{
+res.json(githubdata)
+})
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
